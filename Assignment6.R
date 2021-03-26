6dab2232ab8b1826b80a8bda0af5fc029aabad6a
L24999 <vinaygovekar24@gmail.com>
2021-03-26 20:16:48 +0530

    *3rd Commit*
    *6th Assignment

---
+++ Assignment6.R
@@ -0,0 +1,43 @@
+getwd()
+setwd("/Users/HP/Downloads/Section6-Homework-Data")
+
+mov<-read.csv("Section6-Homework-Data.csv")
+
+head(mov)
+summary(mov)
+str(mov)
+
+library(ggplot2)
+
+s<-ggplot(data=mov, aes(x=Day.of.Week)) + geom_bar()
+
+filt<- (mov$Genre =="action") | (mov$Genre =="adventure") |(mov$Genre =="animation") |(mov$Genre =="comedy") | (mov$Genre =="drama")  
+
+filt2<- mov$Studio  %in% c("Buena Vista Studios", "WB", "Fox", "Universal", "Sony", "Paramount Pictures")
+
+mov2<- mov[filt & filt2,]
+
+p<- ggplot(data=mov2, aes(x=Genre, y=Gross...US))
+
+q<-p + geom_jitter(aes(size=Budget...mill., colour=Studio)) + 
+  geom_boxplot(alpha=0.7, outlier.color = NA)
+
+q<- q + xlab("Genre") + ylab("Gross % US") + ggtitle("Domestic Gross % by Genre")
+
+q<- q + theme(
+  axis.title.x = element_text(colour = "Blue", size = 30),
+  axis.title.y = element_text(colour = "Blue", size = 30),
+  axis.text.x = element_text(size = 20),
+  axis.text.y = element_text(size = 20),
+  
+  plot.title = element_text(size=40),
+  
+  legend.title = element_text(size = 20),
+  legend.text = element_text(size = 20),
+  
+  text = element_text(family = "Comic Sans MS")
+)
+
+q$labels$size <- "Budget $M"
+
+print(q)
